352a9cc038334cb0e7d592e31b5374ab87f31ac0
ANNY <97anny79@gmail.com>
2019-03-29 23:02:26 +0300

    teacher login

---
+++ Teacher-frotntend/src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { Navbar, Button, Nav, NavItem } from 'react-bootstrap';
+import './App.css';
+import Home from './Home/Home';
+
+class App extends Component {
+  goTo(route) {
+    this.props.history.replace(`/${route}`)
+  }
+
+  login() {
+    this.props.auth.login();
+  }
+
+  logout() {
+    this.props.auth.logout();
+  }
+
+  render() {
+    const { isAuthenticated } = this.props.auth;
+
+    return (
+      <div>
+        <Navbar fluid>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#">EMon - Teacher</a>
+            </Navbar.Brand>
+            
+          </Navbar.Header>
+
+            <Nav pullRight>
+                {!isAuthenticated() &&
+                <NavItem onClick={this.login.bind(this)}>Log In</NavItem>}
+                {isAuthenticated() &&
+                <NavItem onClick={this.logout.bind(this)}>Log Out</NavItem>}
+            </Nav>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ Teacher-frotntend/src/Auth/auth0-variables.js
@@ -0,0 +1,5 @@
+export const AUTH_CONFIG = {
+  domain: 'e-mon.eu.auth0.com',
+  clientId: 'SsRVnDbnXPTGjKvOOmf7sayAMRd2KCdz',
+  callbackUrl: 'http://localhost:3000/callback'
+}
