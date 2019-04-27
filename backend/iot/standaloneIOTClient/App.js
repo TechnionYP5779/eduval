9f449e8c82ff390768a63ffe75b52d166ee43caf
Eric <kiel.eric11@gmail.com>
2019-04-28 00:09:18 +0300

    Added a module wrapping the IOT client, and tested it works from within React.

---
+++ backend/iot/standaloneIOTClient/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const notifications = require('./Notifications')
+
+//Just so the messages change
+let counter = 0
+
+function App() {
+	notifications.connect().then(() => {
+		notifications.client.subscribe('someTopic');
+		notifications.client.publish('someTopic', 'saying stuff ' + counter++);
+		notifications.client.on('message', (topic, message) => {
+			console.log("topic: " + topic)
+			console.log("message: " + message)
+			notifications.client.publish('bla', 'saying stuff ' + counter++);
+		})
+	})
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
