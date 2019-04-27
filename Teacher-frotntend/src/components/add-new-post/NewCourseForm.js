0305b3c127d8e34d49117b5ef4d19663b4a48b48
ANNY <97anny79@gmail.com>
2019-04-27 16:03:44 +0300

    added call to add new course for the teacher closes #22

--- Teacher-frotntend/src/components/add-new-post/NewCourseForm.js
+++ Teacher-frotntend/src/components/add-new-post/NewCourseForm.js
@@ -9,57 +9,134 @@ import {
   FormGroup,
   FormCheckbox,
   FormSelect,
-  Button
+  Button,
+  Alert
 } from "shards-react";
+import server from "../../Server/Server";
 
-const NewCourseForm = () => (
-  <ListGroup flush>
-    <ListGroupItem className="p-3">
-      <Row>
-        <Col>
-          <Form>
-            <FormGroup>
-              <label htmlFor="feInputAddress">Course Name</label>
-              <FormInput id="feInputAddress" placeholder="A name that you're students will recognize" />
-            </FormGroup>
 
-            <FormGroup>
-              <label htmlFor="feInputAddress">Course Description</label>
-              <FormInput id="feInputAddress" placeholder="A short explanation of the course" />
-            </FormGroup>
+export default class NewCourseForm extends React.Component {
 
-            <Row form>
-              <Col md="6" className="form-group">
-                <label htmlFor="feEmailAddress">Start Date</label>
-                <FormInput
-                  id="feEmailAddress"
-                  type="date"
-                  placeholder="1/1/19"
-                />
-              </Col>
-              <Col md="6">
-                <label htmlFor="fePassword">End Date</label>
-                <FormInput
-                  id="fePassword"
-                  type="date"
-                  placeholder="1/1/19"
-                />
-              </Col>
-            </Row>
+  constructor(props){
+    super(props);
+    this.state = {
+      name: "",
+      location: "",
+      description: "",
+      startDate: "",
+      endDate: "",
+      disabled: false
+    };
+    this.updateName = this.updateName.bind(this);
+    this.updateDescription = this.updateDescription.bind(this);
+    this.updateLocation = this.updateLocation.bind(this);
+    this.updateStartDate = this.updateStartDate.bind(this);
+    this.updateEndDate = this.updateEndDate.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-            <FormGroup>
-              <label htmlFor="feInputAddress2">Class Room</label>
-              <FormInput
-                id="feInputAddress2"
-                placeholder="Help you're students find the class room"
-              />
-            </FormGroup>
-            <Button type="submit">Create New Course</Button>
-          </Form>
-        </Col>
-      </Row>
-    </ListGroupItem>
-  </ListGroup>
-);
+  updateName(evnt){
+    this.setState({name: evnt.target.value});
+  }
 
-export default NewCourseForm;
+  updateDescription(evnt){
+    this.setState({description: evnt.target.value});
+  }
+
+  updateLocation(evnt){
+    this.setState({location: evnt.target.value});
+  }
+
+  updateStartDate(evnt){
+    this.setState({startDate: evnt.target.value});
+  }
+
+  updateEndDate(evnt){
+    this.setState({endDate: evnt.target.value});
+  }
+
+  handleSubmit(){
+    let checkFilled = (input) => (input == null || input == "" || !input);
+
+
+    let self = this;
+    this.setState({disabled: true});
+    console.log('Your input value is: ', this.state)
+    let handler  =   this.props.handler;
+    if (checkFilled(this.state.name) || checkFilled(this.state.description) ||
+      checkFilled(this.state.location) || checkFilled(this.state.startDate) ||
+      checkFilled(this.state.endDate)){
+      handler("Please fill all forms!");
+      self.setState({disabled: false});
+      return;
+    }
+    server.createNewCourse(function(response){
+      console.log("ok");
+      handler(null);
+      self.setState({disabled: false});
+    }, function(error){
+      console.log(error);
+      handler("An error has occured");
+      self.setState({disabled: false});
+    }, {name: this.state.name, location: this.state.location,
+      description: this.state.description, startDate: this.state.startDate,
+      endDate: this.state.endDate
+    });
+    //handler("Fill all forms!");
+    //Send state to the server code
+  }
+
+  render(){
+    return (
+      <ListGroup flush>
+        <ListGroupItem className="p-3">
+          <Row>
+            <Col>
+              <Form>
+                <FormGroup>
+                  <label htmlFor="feInputAddress">Course Name</label>
+                  {console.log("==============", this, "=================")}
+                  <FormInput id="feInputAddress" placeholder="A name that you're students will recognize" value={this.state.name} onChange={this.updateName} />
+                </FormGroup>
+
+                <FormGroup>
+                  <label htmlFor="feInputAddress">Course Description</label>
+                  <FormInput id="feInputAddress" placeholder="A short explanation of the course" value={this.state.description} onChange={this.updateDescription} />
+                </FormGroup>
+
+                <Row form>
+                  <Col md="6" className="form-group">
+                    <label htmlFor="feEmailAddress">Start Date</label>
+                    <FormInput
+                      id="feEmailAddress"
+                      type="date"
+                      value={this.state.startDate} onChange={this.updateStartDate}
+                    />
+                  </Col>
+                  <Col md="6">
+                    <label htmlFor="fePassword">End Date</label>
+                    <FormInput
+                      id="fePassword"
+                      type="date"
+                      value={this.state.endDate} onChange={this.updateEndDate}
+                    />
+                  </Col>
+                </Row>
+
+                <FormGroup>
+                  <label htmlFor="feInputAddress2">Class Room</label>
+                  <FormInput
+                    id="feInputAddress2"
+                    placeholder="Help you're students find the class room"
+                    value={this.state.location} onChange={this.updateLocation}
+                  />
+                </FormGroup>
+                <Button disabled={this.state.disabled} onClick={this.handleSubmit}>Create New Course</Button>
+              </Form>
+            </Col>
+          </Row>
+        </ListGroupItem>
+      </ListGroup>
+    );
+  }
+}
--- Teacher-frotntend/src/views/AddNewCourse.js
+++ Teacher-frotntend/src/views/AddNewCourse.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Row, Col, Card, CardHeader} from "shards-react";
+import { Container, Row, Col, Card, CardHeader, Alert} from "shards-react";
 
 import CompleteFormExample from "../components/components-overview/CompleteFormExample";
 
@@ -9,23 +9,62 @@ import SidebarActions from "../components/add-new-post/SidebarActions";
 import SidebarCategories from "../components/add-new-post/SidebarCategories";
 import NewCourseForm from "../components/add-new-post/NewCourseForm";
 
+export default class AddNewCourse extends React.Component {
 
-const AddNewCourse = () => (
+  constructor(props){
+    super(props);
+    this.state = {
+      success: false,
+      error: false
+    };
 
-      <Container fluid className="main-content-container px-4 pb-4">
-        {/* Page Header */}
-        <Row noGutters className="page-header py-4">
-          <PageTitle sm="4" title="Add New Course" subtitle="Your Courses" className="text-sm-left" />
-        </Row>
+    this.handler = this.handler.bind(this)
+  }
 
-        <Row>
-          {/* Editor */}
-          <Card style={{width:"80%", marginLeft: "16px"}} >
-            <NewCourseForm />
-          </Card>
-        </Row>
-      </Container>
+  handler(errorMsg){
+    if (errorMsg){
+      this.setState({success: false, error: errorMsg});
+    }
+    else{
+      this.setState({success: true, error: false});
+    }
+  }
 
-);
+  render(){
+    var handler  =   this.handler;
+    return (
+          <div>
+            {this.state.error &&
+            <Container fluid className="px-0">
+              <Alert className="mb-0" theme="danger">
+                <i className="fa fa-info mx-2"></i> {this.state.error}
+              </Alert>
+            </Container>
+            }
+            {this.state.success &&
+            <Container fluid className="px-0">
+              <Alert className="mb-0" theme="success">
+                <i className="fa fa-info mx-2"></i> Success! Your course has been added!
+              </Alert>
+            </Container>
+            }
 
-export default AddNewCourse;
+            <Container fluid className="main-content-container px-4 pb-4">
+
+              {/* Page Header */}
+              <Row noGutters className="page-header py-4">
+                <PageTitle sm="4" title="Add New Course" subtitle="Your Courses" className="text-sm-left" />
+              </Row>
+
+              <Row>
+                {/* Editor */}
+                <Card style={{width:"80%", marginLeft: "16px"}} >
+                  <NewCourseForm handler = {handler.bind(this)}/>
+                </Card>
+              </Row>
+            </Container>
+          </div>
+
+    );
+  }
+}
