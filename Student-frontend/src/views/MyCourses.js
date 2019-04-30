deb7030db2b4140f2b4758186790046a7b7f2797
yanivbaldi <yaniv-b@campus.technion.ac.il>
2019-05-01 01:36:56 +0300

    made join buttons update in realTime according to lesson status

--- Student-frontend/src/views/MyCourses.js
+++ Student-frontend/src/views/MyCourses.js
@@ -14,12 +14,11 @@ import {
 } from "shards-react";
 
 import PageTitle from "../components/common/PageTitle";
+import awsIot  from 'aws-iot-device-sdk';
 
 class MyCourses extends React.Component {
   constructor(props) {
     super(props);
-
-
 
     this.state = {
       id: res,
@@ -43,11 +42,42 @@ class MyCourses extends React.Component {
      this.setState({PostsListThree: response.data});
      
   
-     var i;
-    for (i = 0; i < this.state.PostsListThree.length; i++) { 
-      console.log("number" + i);
-      console.log(this.state.PostsListThree[i].id);
-        axios.get('https://m7zourdxta.execute-api.eu-central-1.amazonaws.com/dev/lesson/'+ this.state.PostsListThree[i].id +'/status',
+           const getContent = function(url) {
+      return new Promise((resolve, reject) => {
+    	    const lib = url.startsWith('https') ? require('https') : require('http');
+    	    const request = lib.get(url, (response) => {
+    	      if (response.statusCode < 200 || response.statusCode > 299) {
+    	         reject(new Error('Failed to load page, status code: ' + response.statusCode));
+    	       }
+
+    	      const body = [];
+    	      response.on('data', (chunk) => body.push(chunk));
+    	      response.on('end', () => resolve(body.join('')));
+    	    });
+    	    request.on('error', (err) => reject(err))
+        })
+    };
+    let client;
+
+      let connect = async () => {
+    	return getContent('https://qh6vsuof2f.execute-api.eu-central-1.amazonaws.com/dev/iot/keys').then((res) => {
+    		res = JSON.parse(res)
+    		client = awsIot.device({
+                region: res.region,
+                protocol: 'wss',
+                accessKeyId: res.accessKey,
+                secretKey: res.secretKey,
+                sessionToken: res.sessionToken,
+                port: 443,
+                host: res.iotEndpoint
+            });
+    	})
+
+    }
+
+    var j;
+    for (j = 0; j < this.state.PostsListThree.length; j++) { 
+      axios.get('https://m7zourdxta.execute-api.eu-central-1.amazonaws.com/dev/lesson/'+ this.state.PostsListThree[j].id +'/status',
           {headers: headers})
           .then((response) => {
             console.log((response));
@@ -66,6 +96,31 @@ class MyCourses extends React.Component {
         }) .catch((error)=>{
           console.log(error);
         });
+    }
+    
+     var i;
+    for (i = 0; i < this.state.PostsListThree.length; i++) { 
+      console.log("number" + i);
+      console.log(this.state.PostsListThree[i].id);
+      let LessonsStatusURL = 'lesson/'+ this.state.PostsListThree[i].id +'/status';  
+      connect().then(() => {
+        client.subscribe(LessonsStatusURL);
+        client.on('message', (topic, message) => {
+          console.log("dsfsdfsdfsdfsdfsdfsdfsdsdfsdf");
+        console.log("message" + message);
+        var current_id = ((topic).split('lesson')[1]).split('/')[1];
+         if(message == "LESSON_START"){
+            var insert = this.state.lessons_status;
+            insert[current_id] = false; 
+            this.setState({lessons_status: insert});
+          }else{
+            var insert = this.state.lessons_status;
+              insert[current_id] = true; 
+             this.setState({lessons_status: insert});
+          }
+        })
+      });
+
   }
 
    })
