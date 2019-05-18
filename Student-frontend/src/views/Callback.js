3dd7896d43ca2b804fad2a8da092f2d27374e6b3
ANNY <97anny79@gmail.com>
2019-05-18 19:31:10 +0300

    use unversal login to avoid 3rd party cookies #98

--- Student-frontend/src/views/Callback.js
+++ Student-frontend/src/views/Callback.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 import loading from './loading.svg';
+import auth from '../Auth/Auth'
+
+import server from "../Server/Server"
 
 class Callback extends Component {
+
+  componentDidMount(){
+    auth.handleAuthentication();
+  }
+
   render() {
     const style = {
       position: 'absolute',
