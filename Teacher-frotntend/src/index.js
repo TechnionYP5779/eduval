e18bee684aad53fa12e32195e753e48b52354fb3
Eric <kiel.eric11@gmail.com>
2019-11-28 12:31:03 +0200

    Also translated MUI tablePagination

--- Teacher-frotntend/src/index.js
+++ Teacher-frotntend/src/index.js
@@ -1,16 +1,12 @@
 import React  from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
-import { enUS, ruRU } from '@material-ui/core/locale';
 
 import * as serviceWorker from './serviceWorker';
 
 import './i18n';
 
-const theme = createMuiTheme({}, ruRU);
-
-ReactDOM.render(<ThemeProvider theme={theme}><App /></ThemeProvider>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
