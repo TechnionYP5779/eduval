d662062081b2579c67d5e5023f705c2e74946849
Eric <kiel.eric11@gmail.com>
2019-12-01 21:30:09 +0200

    fixed some indentations

--- Student-frontend/src/views/CourseDetails.js
+++ Student-frontend/src/views/CourseDetails.js
@@ -87,109 +87,110 @@ class CourseDetails extends React.Component {
       const { t } = this.props;
       const { balance } = this.state;
 
-        return(
-            <Container fluid className="main-content-container px-4 pb-4">
-        {/* Page Header */}
-        <Row noGutters className="page-header py-4">
-          <PageTitle sm="4" title={this.state.courseName} subtitle={t("Course Details")} className="text-sm-left" />
-        </Row>
+      return(
+        <Container fluid className="main-content-container px-4 pb-4">
+          {/* Page Header */}
+          <Row noGutters className="page-header py-4">
+            <PageTitle sm="4" title={this.state.courseName} subtitle={t("Course Details")} className="text-sm-left" />
+          </Row>
 
-
-        <Row>
-          <Col>
-      <Row>
-          {/* Editor */}
-  <Card style = {{height:"100%",width:"100%",marginLeft:"16px"}} className="mb-4">
-    <CardHeader className="border-bottom">
-      <h6 className="m-0">{t("Details")}</h6>
-    </CardHeader>
-    <ListGroup flush>
-      <ListGroupItem className="p-3">
-        <Row>
+          <Row>
             <Col>
-              <Trans i18nKey="emonBalance" count={{balance}}>
-                You have a Balance of <strong>{{balance}}</strong><img alt="Emons" style={{width:"1.5em", marginLeft:"0.2em", marginBottom:"0.2em"}} src={CoinImage} /> in this course.<br /><br />
-                Keep up the good work!
-              </Trans>
+              <Row>
+                {/* Editor */}
+                <Card style = {{height:"100%",width:"100%",marginLeft:"16px"}} className="mb-4">
+                  <CardHeader className="border-bottom">
+                    <h6 className="m-0">{t("Details")}</h6>
+                  </CardHeader>
+                  
+                  <ListGroup flush>
+                    <ListGroupItem className="p-3">
+                      <Row>
+                        <Col>
+                          <Trans i18nKey="emonBalance" count={{balance}}>
+                            You have a Balance of <strong>{{balance}}</strong><img alt="Emons" style={{width:"1.5em", marginLeft:"0.2em", marginBottom:"0.2em"}} src={CoinImage} /> in this course.<br /><br />
+                            Keep up the good work!
+                          </Trans>
+                        </Col>
+                      </Row>
+                    </ListGroupItem>
+                  </ListGroup>
+                </Card>
+              </Row>
+
+              <Row>
+                <Card style = {{height:"100%",width:"100%",marginLeft:"16px"}} className="mb-4">
+                  <CardHeader className="border-bottom">
+                    <h6 className="m-0">{t("Items bought")}</h6>
+                  </CardHeader>
+
+                  <CardBody className="p-0 pb-3">
+                    <table className="table mb-0">
+                      <thead className="bg-light">
+                        <tr>
+                          <th scope="col" className="border-0">
+                            {t("Item")}
+                          </th>
+                          <th scope="col" className="border-0">
+                            {t("Purchase date")}
+                          </th>
+                          <th scope="col" className="border-0">
+                            {t("Price")}
+                          </th>
+                        </tr>
+                      </thead>
+
+                      <tbody>
+                        {Array.from(this.state.ItemsListThree).map((post, idx) => (
+                          <tr>
+                            <td>{post.item.name}</td>
+                            <td>{post.time.padStart(2,'0').split('T')[0] }</td>
+                            <td>{post.item.cost}</td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </table>
+                  </CardBody>
+                </Card>
+              </Row>
+              
+              <Row>
+                {
+                  !this.state.condensed &&
+                  <h2 style = {{margin:"auto"}}>{t("Waiting For Graph")}</h2>
+                }
+                {
+                  this.state.condensed &&
+                  <Card style = {{height:"100%",width:"100%",marginLeft:"16px"}} className="mb-4">
+                    <CardBody className="p-0 pb-3">
+                      <StudentLessonGraph style = {{margin:"auto"}} condensed={this.state.condensed} courseName={this.state.courseName} />
+                    </CardBody>
+                  </Card>
+                }
+              </Row>
             </Col>
-        </Row>
-      </ListGroupItem>
-    </ListGroup>
-  </Card>
-</Row>
-<Row>
-  <Card style = {{height:"100%",width:"100%",marginLeft:"16px"}} className="mb-4">
-    <CardHeader className="border-bottom">
-      <h6 className="m-0">{t("Items bought")}</h6>
-    </CardHeader>
-    <CardBody className="p-0 pb-3">
-            <table className="table mb-0">
-              <thead className="bg-light">
+      
+            <Col>
+              <Card small className="mb-4">
+                <CardHeader className="border-bottom">
+                  <h6 className="m-0">{t("Recent Activity")}</h6>
+                </CardHeader>
 
-                <tr>
-
-                  <th scope="col" className="border-0">
-                    {t("Item")}
-                  </th>
-                  <th scope="col" className="border-0">
-                    {t("Purchase date")}
-                  </th>
-                  <th scope="col" className="border-0">
-                    {t("Price")}
-                  </th>
-
-                </tr>
-              </thead>
-              <tbody>
-              {  Array.from(this.state.ItemsListThree).map((post, idx) => (
-
-                <tr>
-                  <td>{post.item.name}</td>
-                  <td>{post.time.padStart(2,'0').split('T')[0] }</td>
-                  <td>{post.item.cost}</td>
-                </tr>))}
-              </tbody>
-            </table>
-          </CardBody>
-  </Card>
-</Row>
-<Row>
-{
-  !this.state.condensed &&
-  <h2 style = {{margin:"auto"}}>{t("Waiting For Graph")}</h2>
-}
-{
-  this.state.condensed &&
-  <Card style = {{height:"100%",width:"100%",marginLeft:"16px"}} className="mb-4">
-    <CardBody className="p-0 pb-3">
-      <StudentLessonGraph style = {{margin:"auto"}} condensed={this.state.condensed} courseName={this.state.courseName} />
-    </CardBody>
-  </Card>
-}</Row>
-</Col>
-      <Col>
-        <Card small className="mb-4">
-          <CardHeader className="border-bottom">
-            <h6 className="m-0">{t("Recent Activity")}</h6>
-          </CardHeader>
-          <CardBody className="p-0 pb-3">
-            <table className="table mb-0">
-              <thead className="bg-light">
-
-                <tr>
-
-                  <th scope="col" className="border-0">
-                    {t("Day")}
-                  </th>
-                  <th scope="col" className="border-0">
-                    {t("Hour")}
-                  </th>
-                  <th scope="col" className="border-0">
-                    {t("Got")}
-                  </th>
-
-                </tr>
-              </thead>
+                <CardBody className="p-0 pb-3">
+                  <table className="table mb-0">
+                    <thead className="bg-light">
+                      <tr>
+                        <th scope="col" className="border-0">
+                          {t("Day")}
+                        </th>
+                        <th scope="col" className="border-0">
+                          {t("Hour")}
+                        </th>
+                        <th scope="col" className="border-0">
+                          {t("Got")}
+                        </th>
+                      </tr>
+                    </thead>
               <tbody>
               {  Array.from(this.state.PostsListThree).map((post, idx) => (
 
